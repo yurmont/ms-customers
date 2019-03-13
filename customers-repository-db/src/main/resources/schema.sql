2e79bdb844dbc5a90cc8e5086370b4d5104161c9
Ronald Yurivilca <yurmont@gmail.com>
2019-03-13 17:43:40 -0500

    add data scripts

--- customers-repository-db/src/main/resources/schema.sql
+++ customers-repository-db/src/main/resources/schema.sql
@@ -1,1 +1,10 @@
 USE customers;
+
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE `customer` (
+  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,  
+  `birth_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
